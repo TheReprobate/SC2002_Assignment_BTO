868fec5bf7411a2a1e51d1c31ab75de0a3522e5d
JianOon <swah_jian_oon@hotmail.com>
2025-04-06 08:58:01 +0800

    added sample javadoc

--- app/src/main/java/btosystem/App.java
+++ app/src/main/java/btosystem/App.java
@@ -1,6 +1,16 @@
 package btosystem;
 
+/**
+* The main application class serving as the entry point for the program.
+*/
+
 public class App {
+    /**
+    * The main method serving as the program entry point.
+    *
+    * @param args Command-line arguments passed to the application 
+    */
+    
     public static void main(String[] args) {
     }
 }
