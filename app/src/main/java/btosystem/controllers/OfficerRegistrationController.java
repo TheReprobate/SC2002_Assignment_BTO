52fb16f0854ba34c9bb0666f78e8d6b36bcf07c8
TheReprobate <60500130+TheReprobate@users.noreply.github.com>
2025-04-12 16:31:46 +0800

    created ProjectTeamController and OfficerRegistrationController classes

---
+++ app/src/main/java/btosystem/controllers/OfficerRegistrationController.java
@@ -0,0 +1,47 @@
+package btosystem.controllers;
+
+import java.util.List;
+
+import btosystem.classes.HdbOfficer;
+import btosystem.classes.OfficerRegistration;
+import btosystem.classes.Project;
+import btosystem.controllers.interfaces.OfficerRegistrationOperations;
+
+public class OfficerRegistrationController implements OfficerRegistrationOperations{
+
+    @Override
+    public String toString(List<OfficerRegistration> data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+
+    @Override
+    public String toString(OfficerRegistration data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+
+    @Override
+    public OfficerRegistration createRegistration(Project project, HdbOfficer officer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRegistration'");
+    }
+
+    @Override
+    public OfficerRegistration retrieveOfficerRegistration(List<OfficerRegistration> registrations, int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'retrieveOfficerRegistration'");
+    }
+
+    @Override
+    public int approveRegistration(OfficerRegistration registration) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'approveRegistration'");
+    }
+
+    @Override
+    public int rejectRegistration(OfficerRegistration registration) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'rejectRegistration'");
+    }
+}
