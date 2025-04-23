e6ccc69c7c784ed58441755eee46a73cde0fe8fa
TheReprobate <60500130+TheReprobate@users.noreply.github.com>
2025-04-23 11:53:46 +0800

    OfficerRegistration / ProjectTeam and controllers checkstyle fixes

--- app/src/main/java/btosystem/controllers/OfficerRegistrationController.java
+++ app/src/main/java/btosystem/controllers/OfficerRegistrationController.java
@@ -1,21 +1,17 @@
 package btosystem.controllers;
 
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
-import btosystem.classes.ProjectTeam;
-import btosystem.classes.enums.RegistrationStatus;
 import btosystem.classes.HdbOfficer;
 import btosystem.classes.OfficerRegistration;
-
+import btosystem.classes.ProjectTeam;
+import btosystem.classes.enums.RegistrationStatus;
 import btosystem.controllers.interfaces.OfficerRegistrationOperations;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Controller for OfficerRegistration class that implements OfficerRegistrationOperations interface.
  */
-public class OfficerRegistrationController implements OfficerRegistrationOperations
-{
+public class OfficerRegistrationController implements OfficerRegistrationOperations {
     /**
      * Constructor for creating OfficerRegistration.
      *
@@ -23,8 +19,10 @@ public class OfficerRegistrationController implements OfficerRegistrationOperati
      * @param officer Officer that intends to apply for ProjectTeam
      * @return OfficerRegistration object
      */
+
     @Override
-    public OfficerRegistration createRegistration(ProjectTeam team, HdbOfficer officer) throws Exception {
+    public OfficerRegistration createRegistration(ProjectTeam team, 
+                                            HdbOfficer officer) throws Exception {
         
         if (team.getOfficers().stream()
                     .anyMatch(o -> o.equals(officer))) {
@@ -42,33 +40,35 @@ public class OfficerRegistrationController implements OfficerRegistrationOperati
     }
 
     /**
-     * Approves registration for Officer
+     * Approves registration for Officer.
      *
      * @param registrations List of registrations
      * @param index index in list to return
      * @return OfficerRegistration object
      * @throws Exception Exception thrown if registrations does not exist, or index out of bounds
      */
+
     @Override
-    public OfficerRegistration retrieveOfficerRegistration(List<OfficerRegistration> registrations, int index) throws Exception {
-        if (registrations == null)
-        {
+    public OfficerRegistration retrieveOfficerRegistration(List<OfficerRegistration> registrations, 
+                                                        int index) throws Exception {
+        if (registrations == null) {
             throw new Exception("List<OfficerRegistration> object does not exist.");
         }
-        if (index > registrations.size())
-        {
+        if (index > registrations.size()) {
             throw new Exception("Index out of bounds.");
         }
         return registrations.get(index);
     }
 
     /**
-     * Approves registration for Officer
+     * Approves registration for Officer.
      *
      * @param registration Registration object to accept
      * @return 1 if successful, throws relevant error messages if not
-     * @throws Exception Exception thrown if registration does not exist, or registration already accepted
+     * @throws Exception Exception thrown if registration does not exist, 
+     *                                  or registration already accepted
      */
+
     @Override
     public int approveRegistration(OfficerRegistration registration) throws Exception {
         if (registration == null) {
@@ -84,12 +84,14 @@ public class OfficerRegistrationController implements OfficerRegistrationOperati
     }
 
     /**
-     * Rejects registration for Officer
+     * Rejects registration for Officer.
      *
      * @param registration Registration object to reject
      * @return 1 if successful, throws relevant error messages if not
-     * @throws Exception Exception thrown if registration does not exist, or registration already rejected
+     * @throws Exception Exception thrown if registration does not exist, 
+     *                                      or registration already rejected
      */
+
     @Override
     public int rejectRegistration(OfficerRegistration registration) throws Exception {
         if (registration == null) {
@@ -105,11 +107,12 @@ public class OfficerRegistrationController implements OfficerRegistrationOperati
     }
 
     /**
-     * Compiles OfficerRegistrations data into a single string for output
+     * Compiles OfficerRegistrations data into a single string for output.
      *
      * @param data OfficerRegistrations data
      * @return Formatted string with relevant OfficerRegistration data
      */
+
     @Override
     public String toString(OfficerRegistration data) {
         if (data == null) {
@@ -122,18 +125,19 @@ public class OfficerRegistrationController implements OfficerRegistrationOperati
                     data.getStatus().toString());
 
         return String.format("%-4s    %-20s   %-20s\n",
-        "No.", 
-        "Officer Name", 
-        "Application Status")
-        + registration;
+            "No.", 
+            "Officer Name", 
+            "Application Status")
+            + registration;
     }
 
     /**
-     * Compiles list of OfficerRegistrations data into a single string for output
+     * Compiles list of OfficerRegistrations data into a single string for output.
      *
      * @param data List of OfficerRegistrations
      * @return Formatted string with all relevant OfficerRegistrations data
      */
+
     @Override
     public String toString(List<OfficerRegistration> data) {
         if (data == null) {
@@ -155,10 +159,9 @@ public class OfficerRegistrationController implements OfficerRegistrationOperati
         }
 
         return String.format("%-4s    %-20s   %-20s\n",
-         "No.", 
-         "Officer Name", 
-         "Application Status")
-        + registrations;
+             "No.", 
+             "Officer Name", 
+             "Application Status") + registrations;
     }
 
     /**
@@ -166,10 +169,10 @@ public class OfficerRegistrationController implements OfficerRegistrationOperati
      *
      * @param instance OfficerRegistration objects to clean
      */
+
     @Override
     public void cleanup(OfficerRegistration instance) {
-        if (instance != null)
-        {
+        if (instance != null) {
             instance.setOfficer(null);
             instance.setStatus(null);
         }
@@ -180,31 +183,54 @@ public class OfficerRegistrationController implements OfficerRegistrationOperati
      *
      * @param instance List of OfficerRegistration objects to clean
      */
+
     public void cleanup(List<OfficerRegistration> instance) {
-        if (instance != null)
-        {
-            for(OfficerRegistration registration : instance)
-            {
+        if (instance != null) {
+            for (OfficerRegistration registration : instance) {
                 this.cleanup(registration);
             }
             instance.clear();
         }
     }
 
+    /**
+     * Retrieves officer associated with registration object.
+     *
+     * @param registration object to retrieve with
+     * @return HdbOfficer object associated with registration
+     */
+
     @Override
     public HdbOfficer retrieveAppliedOfficer(OfficerRegistration registration) {
         return registration.getOfficer();
     }
 
+    /**
+     * Inserts registration object into registrations list.
+     *
+     * @param registrations list to insert into
+     * @param registration object to insert
+     * @return 1 when successful
+     */
+
     @Override
-    public int addRegistration(List<OfficerRegistration> registrations, OfficerRegistration registration) {
+    public int addRegistration(List<OfficerRegistration> registrations, 
+                                OfficerRegistration registration) {
         registrations.add(registration);
         return 1;
     }
 
+    /**
+     * Checks if officer has already applied for the project.
+     *
+     * @param registrations list to check from
+     * @param officer object to check for
+     * @return true if officer has already applied, false if not
+     */
+
     @Override
     public boolean hasApplied(List<OfficerRegistration> registrations, HdbOfficer officer) {
-        for(OfficerRegistration r: registrations) {
+        for (OfficerRegistration r : registrations) {
             if (!r.getOfficer().equals(officer)) {
                 continue;
             }
@@ -213,9 +239,19 @@ public class OfficerRegistrationController implements OfficerRegistrationOperati
         return false;
     }
 
+    /**
+     * Filters and returns a list of registrations based off status filter.
+     *
+     * @param registrations list to filter from
+     * @param status parameter to filter by
+     * @return list of OfficerRegistrations based off status as filter
+     */
+
     @Override
     public List<OfficerRegistration> filterRegistrations(List<OfficerRegistration> registrations,
             RegistrationStatus status) {
-        return registrations.stream().filter(r -> r.getStatus() == status).collect(Collectors.toList());
+        return registrations.stream()
+                            .filter(r -> r.getStatus() == status)
+                            .collect(Collectors.toList());
     }
 }
--- app/src/main/java/btosystem/controllers/interfaces/ProjectTeamOperations.java
+++ app/src/main/java/btosystem/controllers/interfaces/ProjectTeamOperations.java
@@ -1,12 +1,11 @@
 package btosystem.controllers.interfaces;
 
-import java.util.List;
-
 import btosystem.classes.HdbManager;
 import btosystem.classes.HdbOfficer;
 import btosystem.classes.OfficerRegistration;
 import btosystem.classes.Project;
 import btosystem.classes.ProjectTeam;
+import java.util.List;
 
 /**
  * An interface that extends {@link ToString},
@@ -14,63 +13,74 @@ import btosystem.classes.ProjectTeam;
  */
 public interface ProjectTeamOperations extends 
                 ToString<ProjectTeam>, 
-                CleanupOperations<ProjectTeam>
-{
+                CleanupOperations<ProjectTeam> {
     /**
      * Interface method for ProjectTeam creation.
      * Details can be found in {@link btosystem.controllers.ProjectTeamController}
      */
-    ProjectTeam createProjectTeam(Project project) throws Exception;
 
-    /* -------------------------------------- For HdbManager -------------------------------------- */
+    public ProjectTeam createProjectTeam(Project project) throws Exception;
+
     /**
-     * Interface method for assigning of HdbManager to ProjectTeam
+     * Interface method for assigning of HdbManager to ProjectTeam.
      * Details can be found in {@link btosystem.controllers.ProjectTeamController}
      */
-    // public int assignProjectManager(ProjectTeam team, HdbManager manager, boolean overwrite) throws Exception;
 
     public int assignProject(ProjectTeam team, HdbManager manager) throws Exception;
 
     /**
-     * Interface method to check if HdbManager is part of ProjectTeam
+     * Interface method for assigning of HdbOfficer to ProjectTeam.
      * Details can be found in {@link btosystem.controllers.ProjectTeamController}
      */
-    boolean isInTeam(ProjectTeam team, HdbManager manager) throws Exception;
-    /* -------------------------------------- End HdbManager -------------------------------------- */
     
-    /* -------------------------------------- For HdbOfficer -------------------------------------- */
-    /**
-     * Interface method for assigning of HdbOfficer to ProjectTeam
-     * Details can be found in {@link btosystem.controllers.ProjectTeamController}
-     */
-    // int assignProjectOfficer(ProjectTeam team, HdbOfficer officer) throws Exception;
-    int assignProject(ProjectTeam team, HdbOfficer officer) throws Exception;
+    public int assignProject(ProjectTeam team, HdbOfficer officer) throws Exception;
 
     /**
-     * Interface method to check if HdbManager is part of ProjectTeam
+     * Interface method to check if HdbManager is part of ProjectTeam.
      * Details can be found in {@link btosystem.controllers.ProjectTeamController}
      */
-    boolean isInTeam(ProjectTeam team, HdbOfficer officer) throws Exception;
-    /* -------------------------------------- End HdbOfficer -------------------------------------- */
 
-    /* ---------------------------------- For OfficerRegistration --------------------------------- */
+    public boolean isInTeam(ProjectTeam team, HdbManager manager) throws Exception;
+
     /**
-     * Interface method for assigning of OfficerRegistration to ProjectTeam
+     * Interface method to check if HdbManager is part of ProjectTeam.
      * Details can be found in {@link btosystem.controllers.ProjectTeamController}
      */
+
+    public boolean isInTeam(ProjectTeam team, HdbOfficer officer) throws Exception;
+
+    /**
+     * Interface method for assigning of OfficerRegistration to ProjectTeam.
+     * Details can be found in {@link btosystem.controllers.ProjectTeamController}
+     */
+
     public int addRegistration(ProjectTeam team, OfficerRegistration registration) throws Exception;
 
     /**
-     * Interface method for retrieval of OfficerRegistrations by ProjectTeam object
+     * Interface method for retrieval of OfficerRegistrations by ProjectTeam object.
      * Details can be found in {@link btosystem.controllers.ProjectTeamController}
      */
-    List<OfficerRegistration> retrieveOfficerRegistrations(ProjectTeam team);
-    
-    Project retrieveAssignedProject(ProjectTeam team);
 
-    boolean hasMaxOfficers(ProjectTeam team);
+    public List<OfficerRegistration> retrieveOfficerRegistrations(ProjectTeam team);
     
-    boolean hasManager(ProjectTeam team);
+    /**
+     * Interface method for retrieval of Project by ProjectTeam object.
+     * Details can be found in {@link btosystem.controllers.ProjectTeamController}
+     */
 
-    /* ---------------------------------- End OfficerRegistration --------------------------------- */
+    public Project retrieveAssignedProject(ProjectTeam team);
+
+    /**
+     * Interface method to check if team is at max officers occupancy.
+     * Details can be found in {@link btosystem.controllers.ProjectTeamController}
+     */
+
+    public boolean hasMaxOfficers(ProjectTeam team);
+    
+    /**
+     * Interface method to check if team has manager assigned.
+     * Details can be found in {@link btosystem.controllers.ProjectTeamController}
+     */
+
+    public boolean hasManager(ProjectTeam team);
 }
