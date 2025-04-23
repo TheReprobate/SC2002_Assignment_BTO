abe6a174c01a3e060335aeeaf4cc0b7d51fd15cd
Kaiwei <yapkaiweisg@gmail.com>
2025-04-23 22:14:14 +0800

    Small file path fix

--- app/src/main/java/btosystem/utils/CsvParser.java
+++ app/src/main/java/btosystem/utils/CsvParser.java
@@ -15,7 +15,8 @@ import btosystem.classes.Applicant;
 public class CsvParser {
     public static List<List<String>> loadFromCSV(String filename) {
         List<List<String>> records = new ArrayList<>();
-        File f = new File(String.format("app\\src\\main\\resources\\%s", filename));
+        File f = new File(String.format("src\\main\\resources\\%s", filename));
+        System.out.println(f.getAbsoluteFile());
         if(!(f.exists() && !f.isDirectory())) { 
             try {
                 f.createNewFile();
@@ -48,7 +49,7 @@ public class CsvParser {
     }
 
     public static void saveToCSV(String filename, List<String> strings) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(String.format("app\\src\\main\\resources\\%s", filename)))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(String.format("src\\main\\resources\\%s", filename)))) {
             for(String s: strings){
                 writer.write(s);
                 writer.newLine();
