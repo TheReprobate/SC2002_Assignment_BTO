71a2f65acca0bca6cdc25b317c742f3adf92c3b0
TheReprobate <60500130+TheReprobate@users.noreply.github.com>
2025-04-24 16:35:14 +0800

    More fixes

--- app/src/main/java/btosystem/clients/hdbmanager/HdbManagerCurrentProjectController.java
+++ app/src/main/java/btosystem/clients/hdbmanager/HdbManagerCurrentProjectController.java
@@ -1,8 +1,5 @@
 package btosystem.clients.hdbmanager;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import btosystem.classes.HdbManager;
 import btosystem.classes.OfficerRegistration;
 import btosystem.classes.Project;
@@ -14,22 +11,26 @@ import btosystem.service.HdbManagerServiceManager;
 import btosystem.utils.InputHandler;
 import btosystem.utils.ListToStringFormatter;
 import btosystem.utils.RegexPatterns;
+import java.util.List;
 
-// missing - view enquiry/ view applications/ view team... + filters
-// missing - delete project
 public class HdbManagerCurrentProjectController extends HdbManagerProjectController {
-    private static final String[] MENU = {"View Project Details", "Approve Officer Registration", "Reject Officer Registration", "Edit Project", "Delete Project", "Exit"};
-    private static final String[] EDIT_FIELDS = {"Neighborhood", "Flat Count", "Visibility", "Exit"};
+    private static final String[] MENU = {"View Project Details", 
+                                          "Approve Officer Registration", 
+                                          "Reject Officer Registration", 
+                                          "Edit Project", "Delete Project", "Exit"};
+    private static final String[] EDIT_FIELDS = {"Neighborhood", 
+                                                 "Flat Count", "Visibility", "Exit"};
     private List<Project> projects;
 
-    public HdbManagerCurrentProjectController(HdbManager user, HdbManagerServiceManager serviceManager) {
+    public HdbManagerCurrentProjectController(HdbManager user, 
+                                            HdbManagerServiceManager serviceManager) {
         super(user, serviceManager);
     }
 
     @Override
     protected boolean load() throws Exception {
         projects = serviceManager.getProjectService().getCurrentProject(user);
-        if(projects == null || projects.size() <= 0) {
+        if (projects == null || projects.size() <= 0) {
             throw new Exception("No current project found, join a team. ");
         }
         return true;
@@ -37,7 +38,8 @@ public class HdbManagerCurrentProjectController extends HdbManagerProjectControl
 
     @Override
     protected String display() {
-        return serviceManager.getGenericService().displayProject(projects) + ListToStringFormatter.toString(MENU);
+        return serviceManager.getGenericService().displayProject(projects) 
+            + ListToStringFormatter.toString(MENU);
     }
 
     @Override
@@ -56,10 +58,10 @@ public class HdbManagerCurrentProjectController extends HdbManagerProjectControl
               editProject(); 
               return 0;
           case 4: 
-               deleteProject();
-               return -1;
+              deleteProject();
+              return -1;
           case 5: 
-               return -1;
+              return -1;
           default: 
               throw new Exception("Please enter a valid input. ");
         }
@@ -73,7 +75,9 @@ public class HdbManagerCurrentProjectController extends HdbManagerProjectControl
     private void approveRegistration() throws Exception {
         Project project = getProject();
         OfficerRegistration registration = getRegistration(project);
-        String input = InputHandler.getStringInput("Confirm to approve registration [Y/N]: ", RegexPatterns.YES_NO);
+        String input = InputHandler
+                    .getStringInput("Confirm to approve registration [Y/N]: ", 
+                    RegexPatterns.YES_NO);
         if (!(input.equals("Y") || input.equals("y"))) {
             return;
         }
@@ -85,7 +89,9 @@ public class HdbManagerCurrentProjectController extends HdbManagerProjectControl
     private void rejectRegistration() throws Exception {
         Project project = getProject();
         OfficerRegistration registration = getRegistration(project);
-        String input = InputHandler.getStringInput("Confirm to reject registration [Y/N]: ", RegexPatterns.YES_NO);
+        String input = InputHandler
+                    .getStringInput("Confirm to reject registration [Y/N]: ", 
+                    RegexPatterns.YES_NO);
         if (!(input.equals("Y") || input.equals("y"))) {
             return;
         }
@@ -94,40 +100,43 @@ public class HdbManagerCurrentProjectController extends HdbManagerProjectControl
         System.out.println("Registration Rejected!");
     }
 
-
     private void editProject() throws Exception {
         Project project = getProject();
         System.out.println(ListToStringFormatter.toString(EDIT_FIELDS));
         int index = InputHandler.getIntIndexInput("Select a field: ");
-        switch(index) {
-            case 0: 
-                Neighborhood neighborhood = getInputNeighborhood();
-                serviceManager.getProjectService().editProject(project, neighborhood);
-                System.out.println("Neighborhood edit successful");
-                break;
-            case 1:      
-                FlatType flat = getInputFlat();
-                int count = InputHandler.getIntInput("Input flat count: ");
-                serviceManager.getProjectService().editProject(project, flat, count);
-                System.out.println("Flat count edit successful");
-                break;
-            case 2:
-                String input = InputHandler.getStringInput("Turn visibility on? [Y=on/N=off]: ", RegexPatterns.YES_NO);
-                if(!(input.equals("Y") || input.equals("y"))){
-                    serviceManager.getProjectService().editProject(project, false);
-                    break;
-                }
-                serviceManager.getProjectService().editProject(project, true);
-                break;
-            case 3:
-                break;
-            default:
-                throw new Exception("Option does not exist. ");
+        switch (index) {
+          case 0: 
+              Neighborhood neighborhood = getInputNeighborhood();
+              serviceManager.getProjectService().editProject(project, neighborhood);
+              System.out.println("Neighborhood edit successful");
+              break;
+          case 1:      
+              FlatType flat = getInputFlat();
+              int count = InputHandler.getIntInput("Input flat count: ");
+              serviceManager.getProjectService().editProject(project, flat, count);
+              System.out.println("Flat count edit successful");
+              break;
+          case 2:
+              String input = InputHandler.getStringInput(
+                  "Turn visibility on? [Y=on/N=off]: ", RegexPatterns.YES_NO);
+              if (!(input.equals("Y") || input.equals("y"))) {
+                  serviceManager.getProjectService().editProject(project, false);
+                  break;
+              }
+              serviceManager.getProjectService().editProject(project, true);
+              break;
+          case 3:
+              break;
+          default:
+              throw new Exception("Option does not exist. ");
         }
     }
+
     private OfficerRegistration getRegistration(Project project) throws Exception {
-        List<OfficerRegistration> registrations = serviceManager.getTeamService().getRegistrations(project, RegistrationStatus.PENDING);
-        if(registrations == null || registrations.size() <= 0) {
+        List<OfficerRegistration> registrations = serviceManager.getTeamService()
+                                                                .getRegistrations(project, 
+                                                                RegistrationStatus.PENDING);
+        if (registrations == null || registrations.size() <= 0) {
             throw new Exception("No officer registrations found in this project. ");
         }
         System.out.println(serviceManager.getGenericService().displayRegistration(registrations));
@@ -139,8 +148,9 @@ public class HdbManagerCurrentProjectController extends HdbManagerProjectControl
 
     private void deleteProject() throws Exception {
         Project project = getProject();
-        String input = InputHandler.getStringInput("Confirm to delete project [Y/N]: ", RegexPatterns.YES_NO);
-        if(!(input.equals("Y") || input.equals("y"))){
+        String input = InputHandler.getStringInput(
+                        "Confirm to delete project [Y/N]: ", RegexPatterns.YES_NO);
+        if (!(input.equals("Y") || input.equals("y"))) {
             return;
         }
         serviceManager.getProjectService().deleteProject(user, project);
